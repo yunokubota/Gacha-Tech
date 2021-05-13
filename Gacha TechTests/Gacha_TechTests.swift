be85a54ae40f0b9a5313841ebfeb8a95e0a80f11
kubota yuno <im.kbxynx@2106gmail.com>
2021-05-13 20:03:45 +0900

    Initial Commit

---
+++ Gacha TechTests/Gacha_TechTests.swift
@@ -0,0 +1,33 @@
+//
+//  Gacha_TechTests.swift
+//  Gacha TechTests
+//
+//  Created by 久保田有乃 on 2021/05/13.
+//
+
+import XCTest
+@testable import Gacha_Tech
+
+class Gacha_TechTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
